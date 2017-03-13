74819633136adaba7dfede34a39fdf554c90a476
Sergey Archipov <sergey.archipov.jp@gmail.com>
2017-03-13 13:08:47 +0200

    Main changes, for Name input and message output

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,7 +5,10 @@
 
 public class HelloWorld {
 	public static void main(String[] args) {
+		Input input = new Input();
 		System.out.println("HELLO UGLY WORLD!");
-          System.out.println("The World is not ugly!");
+          System.out.println("The World is not ugly!");		
+		input.ivesti();
+
 	}
 }
