d0fe5fe3cf29b57f1bb29e417083c1aeeda56bc6
Sergey Archipov <sergey.archipov.jp@gmail.com>
2017-03-13 13:14:47 +0200

    Main changed. Now main display date

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,10 +2,12 @@
  * @author zwaclaw
  *
  */
-
+import java.util.*;
 public class HelloWorld {
 	public static void main(String[] args) {
+		Date today = Calendar.getInstance().getTime();
 		System.out.println("HELLO UGLY WORLD!");
           System.out.println("The World is not ugly!");
+		System.out.println(today);
 	}
 }
