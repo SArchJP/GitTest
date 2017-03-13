539fc4246f5db3ec12e865442b4f9a09ffc9208e
Sergey Archipov <sergey.archipov.jp@gmail.com>
2017-03-13 12:57:37 +0200

    New Input.class for Input functionality

---
+++ Input.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Input{
+Scanner reader = new Scanner(System.in);
+
+public void ivesti(){
+System.out.println("Iveskite ka nors: ");
+String input = reader.nextLine();
+}
+
+}
